5bdf05652734c5389685459dfa0df7093ba805b9
Millba <75423614+Millba@users.noreply.github.com>
2025-02-18 00:58:34 +0900

    Update scripts style

--- SQAFD/CIFAR/scripts/cifar100/run_resnet32_afq.sh
+++ SQAFD/CIFAR/scripts/cifar100/run_resnet32_afq.sh
@@ -24,7 +24,7 @@ METHOD_TYPE=$1
 echo $METHOD_TYPE
 
 
-if [ $METHOD_TYPE == "AFQ_T1_W1A1_use_student_quant_params/"]
+if [ $METHOD_TYPE == "AFQ_1bit/"]
 then
     python3 train_quant_with_featureKD.py --gpu_id '0' \
                     --dataset 'cifar100'\
@@ -35,7 +35,6 @@ then
                     --lr_m 5e-4 \
                     --lr_a 5e-4 \
                     --lr_q 5e-6 \
-                    --lr_c 5e-6 \
                     --weight_levels 2 \
                     --act_levels 2 \
                     --feature_levels 2 \
@@ -54,7 +53,7 @@ then
                     --kd_alpha 500\
                     --epochs 720
 
-elif [ $METHOD_TYPE == "AFQ_T2_W2A2_use_student_quant_params/" ] 
+elif [ $METHOD_TYPE == "AFQ_2bit/" ] 
 then
     python3 train_quant_with_featureKD.py --gpu_id '0' \
                     --dataset 'cifar100'\
@@ -65,7 +64,6 @@ then
                     --lr_m 5e-4 \
                     --lr_a 5e-4 \
                     --lr_q 5e-6 \
-                    --lr_c 5e-6 \
                     --weight_levels 4 \
                     --act_levels 4 \
                     --feature_levels 4 \
@@ -84,7 +82,7 @@ then
                     --kd_alpha 500\
                     --epochs 720
 
-elif [ $METHOD_TYPE == "AFQ_T4_W4A4_use_student_quant_params/" ] 
+elif [ $METHOD_TYPE == "AFQ_4bit/" ] 
 then
     python3 train_quant_with_featureKD.py --gpu_id '0' \
                     --dataset 'cifar100'\
@@ -95,7 +93,6 @@ then
                     --lr_m 5e-4 \
                     --lr_a 5e-4 \
                     --lr_q 5e-6 \
-                    --lr_c 5e-6 \
                     --weight_levels 16 \
                     --act_levels 16 \
                     --feature_levels 16 \
@@ -114,96 +111,6 @@ then
                     --kd_alpha 500\
                     --epochs 720
 
-### AFQ + not use_student_quant_params 
-elif [ $METHOD_TYPE == "AFQ_T1_W1A1/" ] 
-then
-    python3 train_quant_with_featureKD.py --gpu_id '0' \
-                    --dataset 'cifar100'\
-                    --arch 'resnet32_quant' \
-                    --num_workers 8 \
-                    --batch_size 64 \
-                    --weight_decay 5e-4 \
-                    --lr_m 5e-4 \
-                    --lr_a 5e-4 \
-                    --lr_q 5e-6 \
-                    --lr_c 5e-6 \
-                    --weight_levels 2 \
-                    --act_levels 2 \
-                    --feature_levels 2 \
-                    --baseline False \
-                    --use_hessian True\
-                    --TFeatureOder 'AFQ' \
-                    --use_student_quant_params False \
-                    --log_dir './results/CIFAR100_ResNet32/'$METHOD_TYPE \
-                    --load_pretrain True \
-                    --pretrain_path './results/CIFAR100_ResNet32/fp_crd_cosine/checkpoint/best_checkpoint.pth' \
-                    --distill 'fd' \
-                    --teacher_path './results/CIFAR100_ResNet32/fp_crd_cosine/checkpoint/best_checkpoint.pth' \
-                    --teacher_arch 'resnet32_fp' \
-                    --train_mode 'student' \
-                    --kd_gamma 1 \
-                    --kd_alpha 500\
-                    --epochs 720
-
-elif [ $METHOD_TYPE == "AFQ_T2_W2A2/" ] 
-then
-    python3 train_quant_with_featureKD.py --gpu_id '0' \
-                    --dataset 'cifar100'\
-                    --arch 'resnet32_quant' \
-                    --num_workers 8 \
-                    --batch_size 64 \
-                    --weight_decay 5e-4 \
-                    --lr_m 5e-4 \
-                    --lr_a 5e-4 \
-                    --lr_q 5e-6 \
-                    --lr_c 5e-6 \
-                    --weight_levels 4 \
-                    --act_levels 4 \
-                    --feature_levels 4 \
-                    --baseline False \
-                    --use_hessian True\
-                    --TFeatureOder 'AFQ' \
-                    --use_student_quant_params False \
-                    --log_dir './results/CIFAR100_ResNet32/'$METHOD_TYPE \
-                    --load_pretrain True \
-                    --pretrain_path './results/CIFAR100_ResNet32/fp_crd_cosine/checkpoint/best_checkpoint.pth' \
-                    --distill 'fd' \
-                    --teacher_path './results/CIFAR100_ResNet32/fp_crd_cosine/checkpoint/best_checkpoint.pth' \
-                    --teacher_arch 'resnet32_fp' \
-                    --train_mode 'student' \
-                    --kd_gamma 1 \
-                    --kd_alpha 500\
-                    --epochs 720
-
-elif [ $METHOD_TYPE == "AFQ_T4_W4A4/" ] 
-then
-    python3 train_quant_with_featureKD.py --gpu_id '0' \
-                    --dataset 'cifar100'\
-                    --arch 'resnet32_quant' \
-                    --num_workers 8 \
-                    --batch_size 64 \
-                    --weight_decay 5e-4 \
-                    --lr_m 5e-4 \
-                    --lr_a 5e-4 \
-                    --lr_q 5e-6 \
-                    --lr_c 5e-6 \
-                    --weight_levels 16 \
-                    --act_levels 16 \
-                    --feature_levels 16 \
-                    --baseline False \
-                    --use_hessian True\
-                    --TFeatureOder 'AFQ' \
-                    --use_student_quant_params False \
-                    --log_dir './results/CIFAR100_ResNet32/'$METHOD_TYPE \
-                    --load_pretrain True \
-                    --pretrain_path './results/CIFAR100_ResNet32/fp_crd_cosine/checkpoint/best_checkpoint.pth' \
-                    --distill 'fd' \
-                    --teacher_path './results/CIFAR100_ResNet32/fp_crd_cosine/checkpoint/best_checkpoint.pth' \
-                    --teacher_arch 'resnet32_fp' \
-                    --train_mode 'student' \
-                    --kd_gamma 1 \
-                    --kd_alpha 500\
-                    --epochs 720
 fi
 
 
